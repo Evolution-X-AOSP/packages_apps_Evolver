9c98296f1aded3ff5a61f04af5b15e1bdaade43c
Alexander Martinz <eviscerationls@gmail.com>
2015-09-09 18:41:45 -0700

    On-The-Go Mode (2/2)
    Change-Id: I6308fbbadac7816bf58f3c8027ed1c738d65f3db

--- src/com/evolution/settings/fragments/PowerMenuSettings.java
+++ src/com/evolution/settings/fragments/PowerMenuSettings.java
@@ -42,6 +42,8 @@ import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.evolution.settings.preferences.Utils;
+import com.evolution.settings.preferences.CustomSeekBarPreference;
 
 public class PowerMenuSettings extends SettingsPreferenceFragment
                 implements Preference.OnPreferenceChangeListener {
@@ -49,11 +51,13 @@ public class PowerMenuSettings extends SettingsPreferenceFragment
     private static final String KEY_POWERMENU_LOGOUT = "powermenu_logout";
     private static final String KEY_POWERMENU_TORCH = "powermenu_torch";
     private static final String KEY_POWERMENU_USERS = "powermenu_users";
+    private static final String PREF_ON_THE_GO_ALPHA = "on_the_go_alpha";
     private static final String SCREEN_OFF_ANIMATION = "screen_off_animation";
 
     private SwitchPreference mPowermenuLogout;
     private SwitchPreference mPowermenuTorch;
     private SwitchPreference mPowermenuUsers;
+    private CustomSeekBarPreference mOnTheGoAlphaPref;
     private ListPreference mScreenOffAnimation;
 
     private UserManager mUserManager;
@@ -95,6 +99,13 @@ public class PowerMenuSettings extends SettingsPreferenceFragment
         mScreenOffAnimation.setValue(Integer.toString(screenOffAnimation));
         mScreenOffAnimation.setSummary(mScreenOffAnimation.getEntry());
         mScreenOffAnimation.setOnPreferenceChangeListener(this);
+
+        mOnTheGoAlphaPref = (CustomSeekBarPreference) findPreference(PREF_ON_THE_GO_ALPHA);
+        float otgAlpha = Settings.System.getFloat(getContentResolver(),
+                Settings.System.ON_THE_GO_ALPHA, 0.5f);
+        final int alpha = ((int) (otgAlpha * 100));
+        mOnTheGoAlphaPref.setValue(alpha);
+        mOnTheGoAlphaPref.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -120,6 +131,11 @@ public class PowerMenuSettings extends SettingsPreferenceFragment
             int index = mScreenOffAnimation.findIndexOfValue((String) newValue);
             mScreenOffAnimation.setSummary(mScreenOffAnimation.getEntries()[index]);
             Settings.Global.putInt(getContentResolver(), Settings.Global.SCREEN_OFF_ANIMATION, value);
+            return true;
+        } else if (preference == mOnTheGoAlphaPref) {
+            float val = (Integer) newValue;
+            Settings.System.putFloat(getActivity().getContentResolver(),
+                    Settings.System.ON_THE_GO_ALPHA, val / 100);
             return true;
         }
         return false;
