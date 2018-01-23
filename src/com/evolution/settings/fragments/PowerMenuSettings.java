651fb824e41cefcd0308f0ad49258c62e309758a
bigrushdog <randall.rushing@gmail.com>
2018-01-23 10:22:20 -0800

    Evolution: Screen off animations [2/2]
    Change-Id: I80c32ce278304ad140285896f0a2ac773d259d22
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- src/com/evolution/settings/fragments/PowerMenuSettings.java
+++ src/com/evolution/settings/fragments/PowerMenuSettings.java
@@ -47,8 +47,10 @@ public class PowerMenuSettings extends SettingsPreferenceFragment
                 implements Preference.OnPreferenceChangeListener {
 
     private static final String KEY_POWERMENU_TORCH = "powermenu_torch";
+    private static final String SCREEN_OFF_ANIMATION = "screen_off_animation";
 
     private SwitchPreference mPowermenuTorch;
+    private ListPreference mScreenOffAnimation;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -67,14 +69,29 @@ public class PowerMenuSettings extends SettingsPreferenceFragment
         mPowermenuTorch.setChecked((Settings.System.getInt(resolver,
                 Settings.System.POWERMENU_TORCH, 0) == 1));
         }
+
+        mScreenOffAnimation = (ListPreference) findPreference(SCREEN_OFF_ANIMATION);
+        int screenOffStyle = Settings.Global.getInt(resolver,
+                Settings.Global.SCREEN_OFF_ANIMATION, 0);
+        mScreenOffAnimation.setValue(String.valueOf(screenOffStyle));
+        mScreenOffAnimation.setSummary(mScreenOffAnimation.getEntry());
+        mScreenOffAnimation.setOnPreferenceChangeListener(this);
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
         if (preference == mPowermenuTorch) {
             boolean value = (Boolean) newValue;
             Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.POWERMENU_TORCH, value ? 1 : 0);
+            return true;
+        } else if (preference == mScreenOffAnimation) {
+            String value = (String) newValue;
+            Settings.Global.putInt(resolver,
+                    Settings.Global.SCREEN_OFF_ANIMATION, Integer.valueOf(value));
+            int valueIndex = mScreenOffAnimation.findIndexOfValue(value);
+            mScreenOffAnimation.setSummary(mScreenOffAnimation.getEntries()[valueIndex]);
             return true;
         }
         return false;
