533405a246e3cab23fc17ec3d43dae9c13275b22
Alex Cruz <du.alexcruz@gmail.com>
2018-04-18 01:31:03 -0700

    Evolver: Add Smart Pixels warning
    Change-Id: Ied06c0ec764c5636c40ee4245336612e0d765e84
    Signed-off-by: Adin Kwok <adin.kwok@carbonrom.org>

--- src/com/evolution/settings/fragments/SmartPixels.java
+++ src/com/evolution/settings/fragments/SmartPixels.java
@@ -52,6 +52,8 @@ public class SmartPixels extends CustomSettingsPreferenceFragment {
 
         addPreferencesFromResource(R.xml.evolution_settings_smart_pixels);
 
+        mFooterPreferenceMixin.createFooterPreference().setTitle(R.string.smart_pixels_warning_text);
+
         addCustomPreference(findPreference(SMART_PIXELS_ENABLE), SYSTEM_TWO_STATE, STATE_OFF);
         addCustomPreference(findPreference(SMART_PIXELS_ON_POWER_SAVE), SYSTEM_TWO_STATE, STATE_OFF);
         mSmartPixelsObserver = new SmartPixelsObserver(new Handler());
