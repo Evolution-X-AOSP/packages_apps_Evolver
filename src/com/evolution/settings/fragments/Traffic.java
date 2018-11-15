05ee9c15c5c96171cc807a2166cac3437964651a
Jon Haus <haus.xda@gmail.com>
2018-11-16 01:36:25 +0530

    Evolver: Add network traffic type [2/2]
    Change-Id: Ib14267df32525439ea67f9b7874a15965a4c5a7c
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- src/com/evolution/settings/fragments/Traffic.java
+++ src/com/evolution/settings/fragments/Traffic.java
@@ -53,6 +53,7 @@ import com.evolution.settings.preference.SystemSettingSwitchPreference;
 public class Traffic extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
 
     private ListPreference mNetTrafficLocation;
+    private ListPreference mNetTrafficType;
     private CustomSeekBarPreference mThreshold;
     private SystemSettingSwitchPreference mShowArrows;
 
@@ -64,6 +65,13 @@ public class Traffic extends SettingsPreferenceFragment implements OnPreferenceC
 
         final ContentResolver resolver = getActivity().getContentResolver();
         final PreferenceScreen prefSet = getPreferenceScreen();
+
+        int type = Settings.System.getIntForUser(resolver,
+                Settings.System.NETWORK_TRAFFIC_TYPE, 0, UserHandle.USER_CURRENT);
+        mNetTrafficType = (ListPreference) findPreference("network_traffic_type");
+        mNetTrafficType.setValue(String.valueOf(type));
+        mNetTrafficType.setSummary(mNetTrafficType.getEntry());
+        mNetTrafficType.setOnPreferenceChangeListener(this);
 
         mNetTrafficLocation = (ListPreference) findPreference("network_traffic_location");
         int location = Settings.System.getIntForUser(resolver,
@@ -125,6 +133,14 @@ public class Traffic extends SettingsPreferenceFragment implements OnPreferenceC
                     Settings.System.NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD, val,
                     UserHandle.USER_CURRENT);
             return true;
+        } else if (preference == mNetTrafficType) {
+            int val = Integer.valueOf((String) objValue);
+            Settings.System.putIntForUser(getContentResolver(),
+                    Settings.System.NETWORK_TRAFFIC_TYPE, val,
+                    UserHandle.USER_CURRENT);
+            int index = mNetTrafficType.findIndexOfValue((String) objValue);
+            mNetTrafficType.setSummary(mNetTrafficType.getEntries()[index]);
+            return true;
         }
         return false;
     }
@@ -134,11 +150,13 @@ public class Traffic extends SettingsPreferenceFragment implements OnPreferenceC
             case 0:
                 mThreshold.setEnabled(false);
                 mShowArrows.setEnabled(false);
+                mNetTrafficType.setEnabled(false);
                 break;
             case 1:
             case 2:
                 mThreshold.setEnabled(true);
                 mShowArrows.setEnabled(true);
+                mNetTrafficType.setEnabled(true);
                 break;
             default:
                 break;
