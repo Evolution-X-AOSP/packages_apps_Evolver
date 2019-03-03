b08753e360159afdef11dddbbffdd47da5a3167a
SuperDroidBond <superdroidbond@yahoo.com>
2019-03-03 23:26:27 +0530

    Evolver: NetworkTraffic: Add Option for Users to select size of Single Traffic (Up/Down) Font Size [2/2]
    spezi77: Adapted to our custom Settings & set default value to 42dp.
    
    Change-Id: Ic9b2d90eda20f36b6c59fdd65ff558d14923624f
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- src/com/evolution/settings/fragments/Traffic.java
+++ src/com/evolution/settings/fragments/Traffic.java
@@ -52,8 +52,11 @@ import com.evolution.settings.preference.SystemSettingSwitchPreference;
 
 public class Traffic extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
 
+    private static final String NETWORK_TRAFFIC_FONT_SIZE  = "network_traffic_font_size";
+
     private ListPreference mNetTrafficLocation;
     private ListPreference mNetTrafficType;
+    private CustomSeekBarPreference mNetTrafficSize;
     private CustomSeekBarPreference mThreshold;
     private SystemSettingSwitchPreference mShowArrows;
 
@@ -65,6 +68,12 @@ public class Traffic extends SettingsPreferenceFragment implements OnPreferenceC
 
         final ContentResolver resolver = getActivity().getContentResolver();
         final PreferenceScreen prefSet = getPreferenceScreen();
+
+        int NetTrafficSize = Settings.System.getInt(resolver,
+                Settings.System.NETWORK_TRAFFIC_FONT_SIZE, 42);
+        mNetTrafficSize = (CustomSeekBarPreference) findPreference(NETWORK_TRAFFIC_FONT_SIZE);
+        mNetTrafficSize.setValue(NetTrafficSize / 1);
+        mNetTrafficSize.setOnPreferenceChangeListener(this);
 
         int type = Settings.System.getIntForUser(resolver,
                 Settings.System.NETWORK_TRAFFIC_TYPE, 0, UserHandle.USER_CURRENT);
@@ -141,6 +150,11 @@ public class Traffic extends SettingsPreferenceFragment implements OnPreferenceC
             int index = mNetTrafficType.findIndexOfValue((String) objValue);
             mNetTrafficType.setSummary(mNetTrafficType.getEntries()[index]);
             return true;
+        }  else if (preference == mNetTrafficSize) {
+            int width = ((Integer)objValue).intValue();
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.NETWORK_TRAFFIC_FONT_SIZE, width);
+            return true;
         }
         return false;
     }
@@ -151,12 +165,14 @@ public class Traffic extends SettingsPreferenceFragment implements OnPreferenceC
                 mThreshold.setEnabled(false);
                 mShowArrows.setEnabled(false);
                 mNetTrafficType.setEnabled(false);
+                mNetTrafficSize.setEnabled(false);
                 break;
             case 1:
             case 2:
                 mThreshold.setEnabled(true);
                 mShowArrows.setEnabled(true);
                 mNetTrafficType.setEnabled(true);
+                mNetTrafficSize.setEnabled(true);
                 break;
             default:
                 break;
