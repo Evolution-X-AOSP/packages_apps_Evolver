6bb78a363c03be9829c639534b02593236789e31
TheStrix <parthbhatia98@gmail.com>
2019-09-01 22:27:12 +0530

    Evolution: Add battery styles and customizations for Android 10 [2/2]
    Allows the user to tune:
    - battery styles
    - battery percentage
    - battery charging text
    
    spezi77:
    - Adapted to our battery styles
    - Note to myself: In Android10 battery percentage has become a global setting
    
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>
    Change-Id: Ibe8784d31acf587d5bc935a0d4b7516c1fe646e7
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

--- src/com/evolution/settings/fragments/StatusBarSettings.java
+++ src/com/evolution/settings/fragments/StatusBarSettings.java
@@ -62,9 +62,22 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener, Indexable {
 
     private static final String SHOW_LTE_FOURGEE = "show_lte_fourgee";
+    private static final String STATUS_BAR_SHOW_BATTERY_PERCENT = "status_bar_show_battery_percent";
+    private static final String STATUS_BAR_BATTERY_TEXT_CHARGING = "status_bar_battery_text_charging";
+    private static final String BATTERY_PERCENTAGE_HIDDEN = "0";
+    private static final String STATUS_BAR_BATTERY_STYLE = "status_bar_battery_style";
 
+    private static final int BATTERY_STYLE_Q = 0;
+    private static final int BATTERY_STYLE_DOTTED_CIRCLE = 1;
+    private static final int BATTERY_STYLE_CIRCLE = 2;
+    private static final int BATTERY_STYLE_TEXT = 3;
+    private static final int BATTERY_STYLE_HIDDEN = 4;
+
+    private ListPreference mBatteryPercent;
+    private ListPreference mBatteryStyle;
     private ListPreference mNetTrafficLocation;
     private CustomSeekBarPreference mThreshold;
+    private SwitchPreference mBatteryCharging;
     private SwitchPreference mShowLteFourGee;
     private SystemSettingSwitchPreference mHideArrows;
 
@@ -97,6 +110,15 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements
         mShowLteFourGee.setChecked((Settings.System.getInt(getContentResolver(),
                 Settings.System.SHOW_LTE_FOURGEE, 0) == 1));
         mShowLteFourGee.setOnPreferenceChangeListener(this);
+
+        mBatteryPercent = (ListPreference) findPreference(STATUS_BAR_SHOW_BATTERY_PERCENT);
+        mBatteryCharging = (SwitchPreference) findPreference(STATUS_BAR_BATTERY_TEXT_CHARGING);
+        mBatteryStyle = (ListPreference) findPreference(STATUS_BAR_BATTERY_STYLE);
+        int batterystyle = Settings.System.getInt(getActivity().getContentResolver(),
+                Settings.System.STATUS_BAR_BATTERY_STYLE, BATTERY_STYLE_Q);
+        mBatteryStyle.setOnPreferenceChangeListener(this);
+
+        updateBatteryOptions(batterystyle);
     }
 
     @Override
@@ -120,8 +142,24 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements
             Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.SHOW_LTE_FOURGEE, value ? 1 : 0);
             return true;
+        } else if (preference == mBatteryStyle) {
+            int value = Integer.parseInt((String) newValue);
+            updateBatteryOptions(value);
+            return true;
         }
         return false;
+    }
+
+    private void updateBatteryOptions(int batterystyle) {
+        boolean enabled = batterystyle != BATTERY_STYLE_TEXT && batterystyle != BATTERY_STYLE_HIDDEN;
+        if (batterystyle == BATTERY_STYLE_HIDDEN) {
+            mBatteryPercent.setValue(BATTERY_PERCENTAGE_HIDDEN);
+            mBatteryPercent.setSummary(mBatteryPercent.getEntry());
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.STATUS_BAR_SHOW_BATTERY_PERCENT, 0);
+        }
+        mBatteryCharging.setEnabled(enabled);
+        mBatteryPercent.setEnabled(enabled);
     }
 
     public void updateTrafficLocation(int location) {
