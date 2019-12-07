4cdab2367327fc2cb78d042fba0ec3928ed7ad5e
Mazda-- <alex@dirtyunicorns.com>
2019-12-07 12:01:37 +0100

    Make powermenu lockscreen visibility configurable [2/2]

--- src/com/evolution/settings/fragments/PowerMenuSettings.java
+++ src/com/evolution/settings/fragments/PowerMenuSettings.java
@@ -51,9 +51,19 @@ import java.util.List;
 public class PowerMenuSettings extends SettingsPreferenceFragment
                 implements Preference.OnPreferenceChangeListener, Indexable {
 
+    private static final String KEY_POWERMENU_LOCKSCREEN = "powermenu_lockscreen";
+    private static final String KEY_POWERMENU_LS_REBOOT = "powermenu_ls_reboot";
+    private static final String KEY_POWERMENU_LS_ADVANCED_REBOOT = "powermenu_ls_advanced_reboot";
+    private static final String KEY_POWERMENU_LS_SCREENSHOT = "powermenu_ls_screenshot";
     private static final String KEY_POWERMENU_TORCH = "powermenu_torch";
+    private static final String KEY_POWERMENU_LS_TORCH = "powermenu_ls_torch";
 
     private SwitchPreference mPowermenuTorch;
+    private SwitchPreference mPowerMenuLockscreen;
+    private SwitchPreference mPowerMenuReboot;
+    private SwitchPreference mPowerMenuAdvancedReboot;
+    private SwitchPreference mPowerMenuScreenshot;
+    private SwitchPreference mPowerMenuLSTorch;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -68,10 +78,38 @@ public class PowerMenuSettings extends SettingsPreferenceFragment
         mPowermenuTorch.setOnPreferenceChangeListener(this);
         if (!Utils.deviceSupportsFlashLight(getActivity())) {
             prefScreen.removePreference(mPowermenuTorch);
+            prefScreen.removePreference(mPowerMenuLSTorch);
         } else {
         mPowermenuTorch.setChecked((Settings.System.getInt(resolver,
                 Settings.System.POWERMENU_TORCH, 0) == 1));
         }
+
+        mPowerMenuLockscreen = (SwitchPreference) findPreference(KEY_POWERMENU_LOCKSCREEN);
+        mPowerMenuLockscreen.setChecked((Settings.System.getInt(getContentResolver(),
+                Settings.System.POWERMENU_LOCKSCREEN, 1) == 1));
+        mPowerMenuLockscreen.setOnPreferenceChangeListener(this);
+
+        mPowerMenuReboot = (SwitchPreference) findPreference(KEY_POWERMENU_LS_REBOOT);
+        mPowerMenuReboot.setChecked((Settings.System.getInt(getContentResolver(),
+                Settings.System.POWERMENU_LS_REBOOT, 1) == 1));
+        mPowerMenuReboot.setOnPreferenceChangeListener(this);
+
+        mPowerMenuAdvancedReboot = (SwitchPreference) findPreference(KEY_POWERMENU_LS_ADVANCED_REBOOT);
+        mPowerMenuAdvancedReboot.setChecked((Settings.System.getInt(getContentResolver(),
+                Settings.System.POWERMENU_LS_ADVANCED_REBOOT, 1) == 1));
+        mPowerMenuAdvancedReboot.setOnPreferenceChangeListener(this);
+
+        mPowerMenuScreenshot = (SwitchPreference) findPreference(KEY_POWERMENU_LS_SCREENSHOT);
+        mPowerMenuScreenshot.setChecked((Settings.System.getInt(getContentResolver(),
+                Settings.System.POWERMENU_LS_SCREENSHOT, 0) == 1));
+        mPowerMenuScreenshot.setOnPreferenceChangeListener(this);
+
+        mPowerMenuLSTorch = (SwitchPreference) findPreference(KEY_POWERMENU_LS_TORCH);
+        mPowerMenuLSTorch.setChecked((Settings.System.getInt(getContentResolver(),
+                Settings.System.POWERMENU_LS_TORCH, 0) == 1));
+        mPowerMenuLSTorch.setOnPreferenceChangeListener(this);
+
+        updateLockscreen();
     }
 
     @Override
@@ -81,6 +119,32 @@ public class PowerMenuSettings extends SettingsPreferenceFragment
             Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.POWERMENU_TORCH, value ? 1 : 0);
             return true;
+        } else if (preference == mPowerMenuLockscreen) {
+            boolean value = (Boolean) newValue;
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.POWERMENU_LOCKSCREEN, value ? 1 : 0);
+            updateLockscreen();
+            return true;
+        } else if (preference == mPowerMenuReboot) {
+            boolean value = (Boolean) newValue;
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.POWERMENU_LS_REBOOT, value ? 1 : 0);
+            return true;
+        } else if (preference == mPowerMenuAdvancedReboot) {
+            boolean value = (Boolean) newValue;
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.POWERMENU_LS_ADVANCED_REBOOT, value ? 1 : 0);
+            return true;
+        } else if (preference == mPowerMenuScreenshot) {
+            boolean value = (Boolean) newValue;
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.POWERMENU_LS_SCREENSHOT, value ? 1 : 0);
+            return true;
+        } else if (preference == mPowerMenuLSTorch) {
+            boolean value = (Boolean) newValue;
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.POWERMENU_LS_TORCH, value ? 1 : 0);
+            return true;
         }
         return false;
     }
@@ -88,6 +152,23 @@ public class PowerMenuSettings extends SettingsPreferenceFragment
     @Override
     public int getMetricsCategory() {
         return MetricsProto.MetricsEvent.EVO_SETTINGS;
+    }
+
+    private void updateLockscreen() {
+        boolean lockscreenOptions = Settings.System.getInt(getActivity().getContentResolver(),
+                Settings.System.POWERMENU_LOCKSCREEN, 1) == 1;
+
+        if (lockscreenOptions) {
+            mPowerMenuReboot.setEnabled(true);
+            mPowerMenuAdvancedReboot.setEnabled(true);
+            mPowerMenuScreenshot.setEnabled(true);
+            mPowerMenuLSTorch.setEnabled(true);
+        } else {
+            mPowerMenuReboot.setEnabled(false);
+            mPowerMenuAdvancedReboot.setEnabled(false);
+            mPowerMenuScreenshot.setEnabled(false);
+            mPowerMenuLSTorch.setEnabled(false);
+        }
     }
 
     public static final SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
