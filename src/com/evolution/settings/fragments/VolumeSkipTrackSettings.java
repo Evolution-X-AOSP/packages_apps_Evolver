396422bcf3201fa1669f9c5489da015c4d7b1fc0
Stallix <team.evolutionx.rom@gmail.com>
2020-01-02 05:15:44 +0000

    Evolution: Add video preview to Skip track gesture option
    * Based on https://github.com/ezio84/abc_packages_apps_Settings/commit/a5a1a58575b5257b44687ba518dd3b38557171fd
    * Use strings from https://github.com/ezio84/abc_packages_apps_Settings/commit/31eb5192ca468de78eca6206f0f3bdf541b892ff
    
    Co-authored-by: ezio84 <brabus84@gmail.com>

---
+++ src/com/evolution/settings/fragments/VolumeSkipTrackSettings.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.evolution.settings.fragments;
+
+import android.os.Bundle;
+
+import androidx.preference.Preference;
+
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class VolumeSkipTrackSettings extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.evolution_settings_volume_rocker_skip_track);
+
+        getActivity().getActionBar().setTitle(R.string.volume_skiptrack_screen_title);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        return false;
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.EVO_SETTINGS;
+    }
+}
