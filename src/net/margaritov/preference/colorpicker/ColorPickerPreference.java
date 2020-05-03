bee8163d350265fd7f950d3d08350b2b693e1331
AnkitGourav <Ankitgourav035@gmail.com>
2020-05-03 13:51:34 +0530

    Evolver: ColorPickerPreference: add function to set custom color preview
    Signed-off-by: AnkitGourav <Ankitgourav035@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- src/net/margaritov/preference/colorpicker/ColorPickerPreference.java
+++ src/net/margaritov/preference/colorpicker/ColorPickerPreference.java
@@ -2,6 +2,7 @@
  * Copyright (C) 2011 Sergey Margaritov
  * Copyright (C) 2013 Slimroms
  * Copyright (C) 2015 The TeamEos Project
+ * Copyright (C) 2020 ion-OS
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -286,6 +287,16 @@ public class ColorPickerPreference extends Preference implements
 
     public void setDefaultColor(int color) {
         mDefValue = color;
+    }
+
+    /**
+     * For custom purposes. Not used by ColorPickerPreference
+     *
+     * set custom color preview from outside
+     * @author ankitgourav
+     */
+    public void setCustomColorPreview(int color) {
+        mValue = color;
     }
 
     /**
