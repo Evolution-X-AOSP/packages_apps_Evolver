8ffb31b0ea79b54d15a2ca11cbd813c4e4fd5cc3
idoybh <idoybh2@gmail.com>
2020-12-04 19:28:44 +0100

    Evolver: Only allow edge lights on AOD when AOD is enabled [2/2]
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- src/com/evolution/settings/fragments/NotificationSettings.java
+++ src/com/evolution/settings/fragments/NotificationSettings.java
@@ -61,6 +61,7 @@ public class NotificationSettings extends SettingsPreferenceFragment implements
     private static final String AMBIENT_LIGHT_DURATION = "ambient_light_duration";
     private static final String AMBIENT_LIGHT_REPEAT_COUNT = "ambient_light_repeat_count";
     private static final String INCALL_VIB_OPTIONS = "incall_vib_options";
+    private static final String KEY_AMBIENT = "ambient_notification_light_enabled";
     private static final String KEY_CHARGING_LIGHT = "charging_light";
     private static final String LED_CATEGORY = "led";
     private static final String NOTIFICATION_HEADERS = "notification_headers";
@@ -73,6 +74,7 @@ public class NotificationSettings extends SettingsPreferenceFragment implements
     private PreferenceCategory mLedCategory;
     private SystemSettingSeekBarPreference mEdgeLightDurationPreference;
     private SystemSettingSeekBarPreference mEdgeLightRepeatCountPreference;
+    private SystemSettingSwitchPreference mAmbientPref;
     private SystemSettingSwitchPreference mNotificationHeader;
 
     @Override
@@ -106,6 +108,15 @@ public class NotificationSettings extends SettingsPreferenceFragment implements
         mNotificationHeader.setChecked((Settings.System.getInt(resolver,
                 Settings.System.NOTIFICATION_HEADERS, 1) == 1));
         mNotificationHeader.setOnPreferenceChangeListener(this);
+
+        mAmbientPref = (SystemSettingSwitchPreference) findPreference(KEY_AMBIENT);
+        boolean aodEnabled = Settings.Secure.getIntForUser(resolver,
+                Settings.Secure.DOZE_ALWAYS_ON, 0, UserHandle.USER_CURRENT) == 1;
+        if (!aodEnabled) {
+            mAmbientPref.setChecked(false);
+            mAmbientPref.setEnabled(false);
+            mAmbientPref.setSummary(R.string.aod_disabled);
+        }
 
         mEdgeLightRepeatCountPreference = (SystemSettingSeekBarPreference) findPreference(AMBIENT_LIGHT_REPEAT_COUNT);
         mEdgeLightRepeatCountPreference.setOnPreferenceChangeListener(this);
