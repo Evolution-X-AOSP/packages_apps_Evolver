12ba691a5e8b9daeff6bfba5c920897d08fbd929
Jackeagle <jackeagle102@gmail.com>
2021-05-27 13:42:29 -0400

    Evolver: Kill AOSP Font options
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    Change-Id: I273045986c00123bd2471af9439b5756912dc188

--- src/com/evolution/settings/fragments/ThemeSettings.java
+++ src/com/evolution/settings/fragments/ThemeSettings.java
@@ -132,8 +132,6 @@ public class ThemeSettings extends DashboardFragment implements OnPreferenceChan
         controllers.add(new OverlayCategoryPreferenceController(context,
                 "android.theme.customization.accent_color"));
         controllers.add(new OverlayCategoryPreferenceController(context,
-                "android.theme.customization.font"));
-        controllers.add(new OverlayCategoryPreferenceController(context,
                 "android.theme.customization.adaptive_icon_shape"));
         controllers.add(new OverlayCategoryPreferenceController(context,
                 "android.theme.customization.icon_pack"));
