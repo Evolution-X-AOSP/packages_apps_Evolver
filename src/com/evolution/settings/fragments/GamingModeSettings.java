4b6d4eac59aea4cab54eba682fccc21f3162ea7f
DennySPb <dennyspb@gmail.com>
2021-09-10 13:57:49 +0300

    Evolver: GamingMode: Option for disable menu overlay [2/3]
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Change-Id: Ia964520b4d68d2f6890adfc787cfb25c0f7c67eb
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- src/com/evolution/settings/fragments/GamingModeSettings.java
+++ src/com/evolution/settings/fragments/GamingModeSettings.java
@@ -18,10 +18,13 @@ package com.evolution.settings.fragments;
 
 import com.android.internal.logging.nano.MetricsProto;
 
+import android.content.ContentResolver;
 import android.content.Context;
 import android.os.Bundle;
 import android.provider.Settings;
 
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceCategory;
 import androidx.preference.SwitchPreference;
 import androidx.preference.PreferenceScreen;
@@ -32,10 +35,15 @@ import com.android.settings.SettingsPreferenceFragment;
 import java.util.ArrayList;
 
 import com.evolution.settings.preference.PackageListPreference;
+import com.evolution.settings.preference.SystemSettingSeekBarPreference;
 
-public class GamingModeSettings extends SettingsPreferenceFragment {
+public class GamingModeSettings extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
 
     private PackageListPreference mGamingPrefList;
+    private SwitchPreference mUseMenuSwitch;
+    private Preference mDanmaku;
+    private Preference mQapps;
+    private SystemSettingSeekBarPreference mOpacity;
 
     private boolean performance_supported;
 
@@ -57,8 +65,37 @@ public class GamingModeSettings extends SettingsPreferenceFragment {
             prefScreen.removePreference(perfCat);
         }
 
+        mUseMenuSwitch = (SwitchPreference) findPreference("gaming_mode_use_overlay_menu");
+        mDanmaku = (Preference) findPreference("gaming_mode_notification_danmaku");
+        mQapps = (Preference) findPreference("gaming_mode_quick_start_apps");
+        mOpacity = (SystemSettingSeekBarPreference) findPreference("gaming_mode_menu_opacity");
+
+        boolean menuEnabled = Settings.System.getInt(getContentResolver(),
+                            Settings.System.GAMING_MODE_USE_OVERLAY_MENU, 1) == 1;
+        mUseMenuSwitch.setChecked(menuEnabled);
+        mUseMenuSwitch.setOnPreferenceChangeListener(this);
+
+        mDanmaku.setEnabled(menuEnabled);
+        mQapps.setEnabled(menuEnabled);
+        mOpacity.setEnabled(menuEnabled);
+
         mGamingPrefList = (PackageListPreference) findPreference("gaming_mode_app_list");
         mGamingPrefList.setRemovedListKey(Settings.System.GAMING_MODE_REMOVED_APP_LIST);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mUseMenuSwitch) {
+            boolean value = (Boolean) newValue;
+            Settings.System.putInt(resolver, Settings.System.GAMING_MODE_USE_OVERLAY_MENU,
+                    value ? 1 : 0);
+            mDanmaku.setEnabled(value);
+            mQapps.setEnabled(value);
+            mOpacity.setEnabled(value);
+            return true;
+        }
+        return false;
     }
 
     @Override
