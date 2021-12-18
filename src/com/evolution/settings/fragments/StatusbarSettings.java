ce0bbb5c4018bc94d2138a49b791bbfb3703cb5f
Pranav Vashi <neobuddy89@gmail.com>
2021-12-18 13:10:32 +0530

    Evolver: Override VoLTE icon with VoWiFi when available [2/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- src/com/evolution/settings/fragments/StatusbarSettings.java
+++ src/com/evolution/settings/fragments/StatusbarSettings.java
@@ -50,6 +50,7 @@ public class StatusbarSettings extends SettingsPreferenceFragment implements
     private static final String CONFIG_RESOURCE_NAME = "flag_combined_status_bar_signal_icons";
     private static final String KEY_VOLTE_ICON_STYLE = "volte_icon_style";
     private static final String KEY_VOWIFI_ICON_STYLE = "vowifi_icon_style";
+    private static final String KEY_VOLTE_VOWIFI_OVERRIDE = "volte_vowifi_override";
     private static final String KEY_SHOW_ROAMING = "roaming_indicator_icon";
     private static final String KEY_SHOW_FOURG = "show_fourg_icon";
     private static final String KEY_SHOW_DATA_DISABLED = "data_disabled_icon";
@@ -68,6 +69,7 @@ public class StatusbarSettings extends SettingsPreferenceFragment implements
 
     private ListPreference mBatteryPercent;
     private ListPreference mBatteryStyle;
+    private SwitchPreference mOverride;
     private SwitchPreference mShowRoaming;
     private SwitchPreference mShowFourg;
     private SwitchPreference mDataDisabled;
@@ -131,6 +133,7 @@ public class StatusbarSettings extends SettingsPreferenceFragment implements
 
         mVolteIconStyle = (SystemSettingSeekBarPreference) findPreference(KEY_VOLTE_ICON_STYLE);
         mVowifiIconStyle = (SystemSettingSeekBarPreference) findPreference(KEY_VOWIFI_ICON_STYLE);
+        mOverride = (SwitchPreference) findPreference(KEY_VOLTE_VOWIFI_OVERRIDE);
         mShowRoaming = (SwitchPreference) findPreference(KEY_SHOW_ROAMING);
         mShowFourg = (SwitchPreference) findPreference(KEY_SHOW_FOURG);
         mDataDisabled = (SwitchPreference) findPreference(KEY_SHOW_DATA_DISABLED);
@@ -139,6 +142,7 @@ public class StatusbarSettings extends SettingsPreferenceFragment implements
         if (!EvolutionUtils.isVoiceCapable(getActivity())) {
             prefScreen.removePreference(mVolteIconStyle);
             prefScreen.removePreference(mVowifiIconStyle);
+            prefScreen.removePreference(mOverride);
             prefScreen.removePreference(mShowRoaming);
             prefScreen.removePreference(mShowFourg);
             prefScreen.removePreference(mDataDisabled);
