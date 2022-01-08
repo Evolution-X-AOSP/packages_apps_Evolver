176470b8c81229189d08770d3ff7000d3e7fb94e
Pranav Vashi <neobuddy89@gmail.com>
2022-01-08 10:36:15 +0530

    Evolver: Change brightness preferences state as required
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/evolution/settings/fragments/QuickSettings.java
+++ src/com/evolution/settings/fragments/QuickSettings.java
@@ -58,7 +58,14 @@ public class QuickSettings extends SettingsPreferenceFragment implements OnPrefe
 
     public static final String TAG = "QuickSettings";
 
+    private static final String KEY_SHOW_BRIGHTNESS_SLIDER = "qs_show_brightness_slider";
+    private static final String KEY_BRIGHTNESS_SLIDER_POSITION = "qs_brightness_slider_position";
+    private static final String KEY_SHOW_AUTO_BRIGHTNESS = "qs_show_auto_brightness";
+
+    private ListPreference mShowBrightnessSlider;
+    private ListPreference mBrightnessSliderPosition;
     private ListPreference mQuickPulldown;
+    private SwitchPreference mShowAutoBrightness;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -66,8 +73,25 @@ public class QuickSettings extends SettingsPreferenceFragment implements OnPrefe
         addPreferencesFromResource(R.xml.evolution_settings_quicksettings);
 
         final Context mContext = getActivity().getApplicationContext();
-        final ContentResolver resolver = getActivity().getContentResolver();
+        final ContentResolver resolver = mContext.getContentResolver();
         final PreferenceScreen prefSet = getPreferenceScreen();
+
+        mShowBrightnessSlider = findPreference(KEY_SHOW_BRIGHTNESS_SLIDER);
+        mShowBrightnessSlider.setOnPreferenceChangeListener(this);
+        boolean showSlider = Settings.Secure.getIntForUser(resolver,
+                Settings.Secure.QS_SHOW_BRIGHTNESS_SLIDER, 1, UserHandle.USER_CURRENT) > 0;
+
+        mBrightnessSliderPosition = findPreference(KEY_BRIGHTNESS_SLIDER_POSITION);
+        mBrightnessSliderPosition.setEnabled(showSlider);
+
+        mShowAutoBrightness = findPreference(KEY_SHOW_AUTO_BRIGHTNESS);
+        boolean automaticAvailable = mContext.getResources().getBoolean(
+                com.android.internal.R.bool.config_automatic_brightness_available);
+        if (automaticAvailable) {
+            mShowAutoBrightness.setEnabled(showSlider);
+        } else {
+            prefSet.removePreference(mShowAutoBrightness);
+        }
 
         int qpmode = Settings.System.getIntForUser(getContentResolver(),
                 Settings.System.STATUS_BAR_QUICK_QS_PULLDOWN, 0, UserHandle.USER_CURRENT);
@@ -80,7 +104,13 @@ public class QuickSettings extends SettingsPreferenceFragment implements OnPrefe
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         ContentResolver resolver = getActivity().getContentResolver();
-        if (preference == mQuickPulldown) {
+        if (preference == mShowBrightnessSlider) {
+            int value = Integer.parseInt((String) newValue);
+            mBrightnessSliderPosition.setEnabled(value > 0);
+            if (mShowAutoBrightness != null)
+                mShowAutoBrightness.setEnabled(value > 0);
+            return true;
+        } else if (preference == mQuickPulldown) {
             int value = Integer.parseInt((String) newValue);
             Settings.System.putIntForUser(resolver,
                     Settings.System.STATUS_BAR_QUICK_QS_PULLDOWN, value,
