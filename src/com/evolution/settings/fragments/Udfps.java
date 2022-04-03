11493805dd4354a9bb31bfa58284fa85a2f3a919
Vachounet <vachounet@live.fr>
2022-04-03 16:16:07 +0300

    Evolver: Custom fp icon

--- src/com/evolution/settings/fragments/Udfps.java
+++ src/com/evolution/settings/fragments/Udfps.java
@@ -16,16 +16,29 @@
  */
 package com.evolution.settings.fragments;
 
+import android.app.Activity;
+import android.app.WallpaperManager;
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Resources;
 import android.database.ContentObserver;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.ParcelFileDescriptor;
 import android.os.SystemProperties;
+import android.os.UserHandle;
 import android.os.UserHandle;
 import android.provider.SearchIndexableResource;
 import android.provider.Settings;
+import android.text.TextUtils;
 import android.view.View;
 
 import androidx.preference.ListPreference;
@@ -37,13 +50,19 @@ import androidx.preference.SwitchPreference;
 
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.android.internal.util.evolution.EvolutionUtils;
+
 import com.android.settings.R;
 import com.android.settings.Utils;
 import com.android.settings.dashboard.DashboardFragment;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settingslib.search.SearchIndexable;
 
+import com.evolution.settings.fragments.UdfpsIconPicker;
+import com.evolution.settings.preference.SystemSettingSwitchPreference;
+
+import java.io.FileDescriptor;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @SearchIndexable
@@ -52,8 +71,14 @@ public class Udfps extends DashboardFragment implements
 
     private static final String TAG = "Udfps";
     private static final String UDFPS_CUSTOMIZATION = "udfps_customization";
+    private static final String CUSTOM_FOD_ICON_KEY = "custom_fp_icon_enabled";
+    private static final String CUSTOM_FP_FILE_SELECT = "custom_fp_file_select";
+    private static final int REQUEST_PICK_IMAGE = 0;
 
     private PreferenceCategory mUdfpsCustomization;
+    private Preference mCustomFPImage;
+    private SystemSettingSwitchPreference mCustomFodIcon;
+    private Preference mUdfpsIconPicker;
 
     @Override
     protected int getPreferenceScreenResId() {
@@ -73,12 +98,83 @@ public class Udfps extends DashboardFragment implements
         if (!udfpsResPkgInstalled) {
             prefScreen.removePreference(mUdfpsCustomization);
         }
+
+        mUdfpsIconPicker = (Preference) prefScreen.findPreference("udfps_icon_picker");
+
+        mCustomFPImage = findPreference(CUSTOM_FP_FILE_SELECT);
+        final String customIconURI = Settings.System.getString(getContext().getContentResolver(),
+               Settings.System.OMNI_CUSTOM_FP_ICON);
+        if (!TextUtils.isEmpty(customIconURI)) {
+            setPickerIcon(customIconURI);
+        }
+
+        mCustomFodIcon = (SystemSettingSwitchPreference) findPreference(CUSTOM_FOD_ICON_KEY);
+        boolean val = Settings.System.getIntForUser(getActivity().getContentResolver(),
+                Settings.System.OMNI_CUSTOM_FP_ICON_ENABLED, 0, UserHandle.USER_CURRENT) == 1;
+        mCustomFodIcon.setOnPreferenceChangeListener(this);
+        if (val) {
+            mUdfpsIconPicker.setEnabled(false);
+        } else {
+            mUdfpsIconPicker.setEnabled(true);
+        }
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mCustomFodIcon) {
+            boolean val = (Boolean) newValue;
+            Settings.System.putIntForUser(getActivity().getContentResolver(),
+                    Settings.System.OMNI_CUSTOM_FP_ICON_ENABLED, val ? 1 : 0,
+                    UserHandle.USER_CURRENT);
+            if (val) {
+                mUdfpsIconPicker.setEnabled(false);
+            } else {
+                mUdfpsIconPicker.setEnabled(true);
+            }
+            return true;
+        }
         return false;
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(Preference preference) {
+        if (preference == mCustomFPImage) {
+            Intent intent = new Intent(Intent.ACTION_PICK);
+            intent.setType("image/*");
+            startActivityForResult(intent, REQUEST_PICK_IMAGE);
+            return true;
+        }
+        return super.onPreferenceTreeClick(preference);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent result) {
+       if (requestCode == REQUEST_PICK_IMAGE && resultCode == Activity.RESULT_OK) {
+           Uri uri = null;
+           if (result != null) {
+               uri = result.getData();
+               setPickerIcon(uri.toString());
+               Settings.System.putString(getContentResolver(), Settings.System.OMNI_CUSTOM_FP_ICON,
+                   uri.toString());
+            }
+        } else if (requestCode == REQUEST_PICK_IMAGE && resultCode == Activity.RESULT_CANCELED) {
+            mCustomFPImage.setIcon(new ColorDrawable(Color.TRANSPARENT));
+            Settings.System.putString(getContentResolver(), Settings.System.OMNI_CUSTOM_FP_ICON, "");
+        }
+    }
+
+    private void setPickerIcon(String uri) {
+        try {
+                ParcelFileDescriptor parcelFileDescriptor =
+                    getContext().getContentResolver().openFileDescriptor(Uri.parse(uri), "r");
+                FileDescriptor fileDescriptor = parcelFileDescriptor.getFileDescriptor();
+                Bitmap image = BitmapFactory.decodeFileDescriptor(fileDescriptor);
+                parcelFileDescriptor.close();
+                Drawable d = new BitmapDrawable(getResources(), image);
+                mCustomFPImage.setIcon(d);
+            }
+            catch (Exception e) {}
     }
 
     @Override
