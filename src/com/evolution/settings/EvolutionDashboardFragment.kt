6eb57ca2b53b4bd5f3b143bb65c16200965a2d65
Joey Huab <joey@evolution-x.org>
2022-08-19 09:40:13 +0000

    Evolver: Refactor for Tiramisu
    Co-authored-by: jhonboy121 <alfredmathew05@gmail.com>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: ReallySnow <priv@reallysnow.moe>
    Co-authored-by: Col_or <col_or@qq.com>
    Co-authored-by: Kujou Yuko <i@779.moe>
    Co-authored-by: Max <xyyx@mail.ru>
    Co-authored-by: maxwen <max.weninger@gmail.com>

---
+++ src/com/evolution/settings/EvolutionDashboardFragment.kt
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2022 FlamingoOS Project
+ *               2019-2022 Evolution X
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.evolution.settings
+
+import androidx.preference.Preference
+
+import com.android.internal.logging.nano.MetricsProto
+import com.android.settings.dashboard.DashboardFragment
+import com.evolution.settings.fragments.ColorPickerFragment
+import com.evolution.settings.preference.ColorPickerPreference
+
+abstract class EvolutionDashboardFragment : DashboardFragment() {
+
+    override fun getMetricsCategory(): Int = MetricsProto.MetricsEvent.EVOLVER
+
+    override fun onDisplayPreferenceDialog(preference: Preference) {
+        if (preference is ColorPickerPreference) {
+            ColorPickerFragment(preference.color).apply {
+                setOnConfirmListener {
+                    preference.setColor(it)
+                }
+            }.show(childFragmentManager, COLOR_PICKER_DIALOG_KEY)
+        } else {
+            super.onDisplayPreferenceDialog(preference)
+        }
+    }
+
+    companion object {
+        const val COLOR_PICKER_DIALOG_KEY = "color_picker_dialog"
+    }
+}
--- src/com/evolution/settings/preference/CustomSettingsPreferenceFragment.java
+++ src/com/evolution/settings/preference/CustomSettingsPreferenceFragment.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2018 CarbonROM
- * Copyright (C) 2018 Adin Kwok
+ *               2018 Adin Kwok
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
