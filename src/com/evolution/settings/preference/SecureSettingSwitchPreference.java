5bee90758f1b2750f3105400af0e746e82bd990b
Joey Huab <joey@evolution-x.org>
2022-08-19 09:40:13 +0000

    Evolver: Refactor for Tiramisu
    Co-authored-by: jhonboy121 <alfredmathew05@gmail.com>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: ReallySnow <priv@reallysnow.moe>
    Co-authored-by: Col_or <col_or@qq.com>
    Co-authored-by: Kujou Yuko <i@779.moe>
    Co-authored-by: Max <xyyx@mail.ru>
    Co-authored-by: maxwen <max.weninger@gmail.com>
    Co-authored-by: minaripenguin37 <alexfinhart@gmail.com>

--- src/com/evolution/settings/preference/SecureSettingSwitchPreference.java
+++ src/com/evolution/settings/preference/SecureSettingSwitchPreference.java
@@ -1,13 +1,11 @@
 /*
- * Copyright (C) 2014 The CyanogenMod project
- * Copyright (C) 2017 AICP
- * Copyright (C) 2019-2020 The Evolution X Project
+ * Copyright (C) 2016-2018 crDroid Android Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,38 +13,42 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.evolution.settings.preference;
 
 import android.content.Context;
-import androidx.preference.SwitchPreference;
+import android.provider.Settings;
+import android.os.UserHandle;
 import android.util.AttributeSet;
 
-public class SecureSettingSwitchPreference extends SwitchPreference {
+import com.evolution.settings.preference.SelfRemovingSwitchPreference;
+
+public class SecureSettingSwitchPreference extends SelfRemovingSwitchPreference {
 
     public SecureSettingSwitchPreference(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
-        setPreferenceDataStore(new SecureSettingsStore(context.getContentResolver()));
     }
 
     public SecureSettingSwitchPreference(Context context, AttributeSet attrs) {
         super(context, attrs);
-        setPreferenceDataStore(new SecureSettingsStore(context.getContentResolver()));
     }
 
     public SecureSettingSwitchPreference(Context context) {
         super(context);
-        setPreferenceDataStore(new SecureSettingsStore(context.getContentResolver()));
     }
 
     @Override
-    protected void onSetInitialValue(boolean restoreValue, Object defaultValue) {
-        // This is what default TwoStatePreference implementation is doing without respecting
-        // real default value:
-        //setChecked(restoreValue ? getPersistedBoolean(mChecked)
-        //        : (Boolean) defaultValue);
-        // Instead, we better do
-        setChecked(restoreValue ? getPersistedBoolean((Boolean) defaultValue)
-                : (Boolean) defaultValue);
+    protected boolean isPersisted() {
+        return Settings.Secure.getString(getContext().getContentResolver(), getKey()) != null;
+    }
+
+    @Override
+    protected void putBoolean(String key, boolean value) {
+        Settings.Secure.putIntForUser(getContext().getContentResolver(), key, value ? 1 : 0, UserHandle.USER_CURRENT);
+    }
+
+    @Override
+    protected boolean getBoolean(String key, boolean defaultValue) {
+        return Settings.Secure.getIntForUser(getContext().getContentResolver(),
+                key, defaultValue ? 1 : 0, UserHandle.USER_CURRENT) != 0;
     }
 }
