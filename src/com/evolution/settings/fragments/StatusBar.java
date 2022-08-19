0b024aa4f8b4e32cc14d02ed324bf0d8a9f8580c
Joey Huab <joey@evolution-x.org>
2022-08-19 09:40:13 +0000

    [TMP] Evolver: Refactor for A13

--- src/com/evolution/settings/fragments/StatusBar.java
+++ src/com/evolution/settings/fragments/StatusBar.java
@@ -17,64 +17,49 @@ package com.evolution.settings.fragments;
 
 import android.content.ContentResolver;
 import android.content.Context;
-import android.content.pm.PackageManager;
-import android.content.pm.PackageManager.NameNotFoundException;
-import android.content.pm.ResolveInfo;
 import android.content.res.Resources;
+import android.database.ContentObserver;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemProperties;
 import android.os.UserHandle;
 import android.provider.Settings;
 import android.view.View;
 
 import androidx.preference.ListPreference;
-import androidx.preference.Preference.OnPreferenceChangeListener;
 import androidx.preference.Preference;
+import androidx.preference.PreferenceCategory;
 import androidx.preference.PreferenceScreen;
+import androidx.preference.Preference.OnPreferenceChangeListener;
 import androidx.preference.SwitchPreference;
 
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
-import com.android.internal.util.evolution.EvolutionUtils;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settingslib.search.SearchIndexable;
 
 import com.evolution.settings.fragments.Clock;
 import com.evolution.settings.preference.SystemSettingListPreference;
-import com.evolution.settings.preference.SystemSettingSeekBarPreference;
-import com.evolution.settings.preference.SystemSettingSwitchPreference;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SearchIndexable(forTarget = SearchIndexable.ALL & ~SearchIndexable.ARC)
 public class StatusBar extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
 
-    private static final String KEY_COMBINED_ICONS = "combined_status_bar_signal_icons";
-    private static final String KEY_VOLTE_ICON_STYLE = "volte_icon_style";
-    private static final String KEY_VOWIFI_ICON_STYLE = "vowifi_icon_style";
-    private static final String KEY_VOLTE_VOWIFI_OVERRIDE = "volte_vowifi_override";
-    private static final String KEY_SHOW_ROAMING = "roaming_indicator_icon";
-    private static final String KEY_SHOW_FOURG = "show_fourg_icon";
-    private static final String KEY_SHOW_DATA_DISABLED = "data_disabled_icon";
     private static final String KEY_STATUS_BAR_SHOW_BATTERY_PERCENT = "status_bar_show_battery_percent";
     private static final String KEY_STATUS_BAR_BATTERY_STYLE = "status_bar_battery_style";
     private static final String KEY_STATUS_BAR_BATTERY_TEXT_CHARGING = "status_bar_battery_text_charging";
-    private static final String KEY_USE_OLD_MOBILETYPE = "use_old_mobiletype";
     private static final String STATUS_BAR_CLOCK_STYLE = "status_bar_clock";
-    private static final String SYSTEMUI_PACKAGE = "com.android.systemui";
 
     private static final int BATTERY_STYLE_PORTRAIT = 0;
     private static final int BATTERY_STYLE_TEXT = 4;
     private static final int BATTERY_STYLE_HIDDEN = 5;
 
     private SwitchPreference mBatteryTextCharging;
-    private SwitchPreference mCombinedIcons;
-    private SwitchPreference mOverride;
-    private SwitchPreference mShowRoaming;
-    private SwitchPreference mShowFourg;
-    private SwitchPreference mDataDisabled;
-    private SwitchPreference mOldMobileType;
-    private SystemSettingSeekBarPreference mVolteIconStyle;
-    private SystemSettingSeekBarPreference mVowifiIconStyle;
     private SystemSettingListPreference mBatteryPercent;
     private SystemSettingListPreference mBatteryStyle;
     private SystemSettingListPreference mStatusBarClock;
@@ -87,26 +72,6 @@ public class StatusBar extends SettingsPreferenceFragment implements
         final ContentResolver resolver = getActivity().getContentResolver();
         final Context mContext = getActivity().getApplicationContext();
         final PreferenceScreen prefScreen = getPreferenceScreen();
-
-        mVolteIconStyle = (SystemSettingSeekBarPreference) findPreference(KEY_VOLTE_ICON_STYLE);
-        mVowifiIconStyle = (SystemSettingSeekBarPreference) findPreference(KEY_VOWIFI_ICON_STYLE);
-        mCombinedIcons = (SwitchPreference) findPreference(KEY_COMBINED_ICONS);
-        mOverride = (SwitchPreference) findPreference(KEY_VOLTE_VOWIFI_OVERRIDE);
-        mShowRoaming = (SwitchPreference) findPreference(KEY_SHOW_ROAMING);
-        mShowFourg = (SwitchPreference) findPreference(KEY_SHOW_FOURG);
-        mDataDisabled = (SwitchPreference) findPreference(KEY_SHOW_DATA_DISABLED);
-        mOldMobileType = (SwitchPreference) findPreference(KEY_USE_OLD_MOBILETYPE);
-
-        if (!EvolutionUtils.isVoiceCapable(getActivity())) {
-            prefScreen.removePreference(mCombinedIcons);
-            prefScreen.removePreference(mVolteIconStyle);
-            prefScreen.removePreference(mVowifiIconStyle);
-            prefScreen.removePreference(mOverride);
-            prefScreen.removePreference(mShowRoaming);
-            prefScreen.removePreference(mShowFourg);
-            prefScreen.removePreference(mDataDisabled);
-            prefScreen.removePreference(mOldMobileType);
-        }
 
         int batterystyle = Settings.System.getIntForUser(getContentResolver(),
                 Settings.System.STATUS_BAR_BATTERY_STYLE, BATTERY_STYLE_PORTRAIT, UserHandle.USER_CURRENT);
