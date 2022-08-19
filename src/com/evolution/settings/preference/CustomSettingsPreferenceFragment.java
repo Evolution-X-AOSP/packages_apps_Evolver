0c9327ebb9878633b63e1d01cd20a23e42fb7dcd
Joey Huab <joey@evolution-x.org>
2022-08-19 09:40:13 +0000

    Evolver: Refactor for Tiramisu
    Co-authored-by: jhonboy121 <alfredmathew05@gmail.com>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: ReallySnow <priv@reallysnow.moe>
    Co-authored-by: Col_or <col_or@qq.com>
    Co-authored-by: Kujou Yuko <i@779.moe>
    Co-authored-by: Max <xyyx@mail.ru>

--- src/com/evolution/settings/preference/CustomSettingsPreferenceFragment.java
+++ src/com/evolution/settings/preference/CustomSettingsPreferenceFragment.java
@@ -1,6 +1,7 @@
 /*
  * Copyright (C) 2018 CarbonROM
- * Copyright (C) 2018 Adin Kwok
+ *               2018 Adin Kwok
+ *               2019-2022 Evolution X
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
