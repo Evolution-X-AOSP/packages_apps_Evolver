ed3eb0d9ad9f6e026bfc17095a5c8b8f78f8b9b6
Joey Huab <joey@evolution-x.org>
2023-02-24 11:14:33 +0000

    Evolver: Bring back Parallel space [4/4]
    * Original commits available in revert squash.
    
    Revert "[SQUASH] Evolver: Remove parallel space [3/3]"
    
    This reverts commit 3eb6693f9de8ac90d5de1103a554b08a190518c6.

---
+++ src/com/evolution/settings/fragments/GmsSwitch.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2023 crDroid Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.evolution.settings.fragments;
+
+import android.content.Context;
+import android.content.ContentResolver;
+import android.content.res.Resources;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.UserHandle;
+import android.provider.Settings;
+
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.SwitchPreference;
+
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.settings.R;
+import com.android.settings.dashboard.DashboardFragment;
+
+public class GmsSwitch extends DashboardFragment {
+
+    private static final String TAG = "GmsSwitch";
+
+    @Override
+    protected int getPreferenceScreenResId() {
+        return R.xml.evolution_settings_gms_enabled;
+    }
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+    }
+
+    @Override
+    protected String getLogTag() {
+        return TAG;
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.EVOLVER;
+    }
+}
