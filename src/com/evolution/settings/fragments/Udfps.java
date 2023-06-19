8cab5245c2d1c95b2e2cace1c39c239092fc8bcb
Joey Huab <joey@evolution-x.org>
2023-06-19 20:51:46 +0900

    Evolver: Changes for A13 QPR3
    * Add changelog icon
    * Re-enable unlock ripple effect toggle
    * Disable lockscreen battery info
    * Disable lockscreen clock format settings
    * Disable lockscreen media artwork blur radius
    * Disable lockscreen media artwork fade percent
    * Disable entire QS Header category
    * Disable notification material dismiss category
    * Disable status bar reticker by default
    * Disable status bar lyric preference
    * Disable old mobile type toggle
    * Disable QS data usage (causes QS stutters)
    * Move QS panel style to QuickSettings fragment
    * Remove Alert slider pocket judge check

--- src/com/evolution/settings/fragments/Udfps.java
+++ src/com/evolution/settings/fragments/Udfps.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2017-2022 The Project-Xtended
- *               2019-2022 Evolution X
+ *               2023 Evolution X
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
