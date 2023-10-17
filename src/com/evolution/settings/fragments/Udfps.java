d369f0de64061bc554f7004efad004c7132d4d25
Joey Huab <joey@evolution-x.org>
2023-10-17 15:05:48 +0900

    Evolver: Upgrade to UDC

--- src/com/evolution/settings/fragments/Udfps.java
+++ src/com/evolution/settings/fragments/Udfps.java
@@ -56,9 +56,6 @@ import com.android.settings.dashboard.DashboardFragment;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settingslib.search.SearchIndexable;
 
-import com.evolution.settings.fragments.UdfpsIconPicker;
-import com.evolution.settings.preference.SystemSettingSwitchPreference;
-
 import java.io.FileDescriptor;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -69,15 +66,6 @@ public class Udfps extends DashboardFragment implements
         Preference.OnPreferenceChangeListener {
 
     private static final String TAG = "Udfps";
-    private static final String UDFPS_CUSTOMIZATION = "udfps_customization";
-    private static final String CUSTOM_FOD_ICON_KEY = "custom_fp_icon_enabled";
-    private static final String CUSTOM_FP_FILE_SELECT = "custom_fp_file_select";
-    private static final int REQUEST_PICK_IMAGE = 0;
-
-    private PreferenceCategory mUdfpsCustomization;
-    private Preference mCustomFPImage;
-    private SystemSettingSwitchPreference mCustomFodIcon;
-    private Preference mUdfpsIconPicker;
 
     @Override
     protected int getPreferenceScreenResId() {
@@ -90,91 +78,12 @@ public class Udfps extends DashboardFragment implements
 
         final PreferenceScreen prefScreen = getPreferenceScreen();
         Resources resources = getResources();
-
-        final boolean udfpsResPkgInstalled = EvolutionUtils.isPackageInstalled(getContext(),
-                "com.evolution.udfps.resources");
-	mUdfpsCustomization = (PreferenceCategory) findPreference(UDFPS_CUSTOMIZATION);
-        if (!udfpsResPkgInstalled) {
-            prefScreen.removePreference(mUdfpsCustomization);
-        }
-
-        mUdfpsIconPicker = (Preference) prefScreen.findPreference("udfps_icon_picker");
-
-        mCustomFPImage = findPreference(CUSTOM_FP_FILE_SELECT);
-        final String customIconURI = Settings.System.getString(getContext().getContentResolver(),
-               Settings.System.OMNI_CUSTOM_FP_ICON);
-        if (!TextUtils.isEmpty(customIconURI)) {
-            setPickerIcon(customIconURI);
-        }
-
-        mCustomFodIcon = (SystemSettingSwitchPreference) findPreference(CUSTOM_FOD_ICON_KEY);
-        boolean val = Settings.System.getIntForUser(getActivity().getContentResolver(),
-                Settings.System.OMNI_CUSTOM_FP_ICON_ENABLED, 0, UserHandle.USER_CURRENT) == 1;
-        if (mCustomFodIcon != null) {
-            mCustomFodIcon.setOnPreferenceChangeListener(this);
-            if (val) {
-                mUdfpsIconPicker.setEnabled(false);
-            } else {
-                mUdfpsIconPicker.setEnabled(true);
-            }
-        }
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         ContentResolver resolver = getActivity().getContentResolver();
-        if (preference == mCustomFodIcon) {
-            boolean val = (Boolean) newValue;
-            Settings.System.putIntForUser(getActivity().getContentResolver(),
-                    Settings.System.OMNI_CUSTOM_FP_ICON_ENABLED, val ? 1 : 0,
-                    UserHandle.USER_CURRENT);
-            if (val) {
-                mUdfpsIconPicker.setEnabled(false);
-            } else {
-                mUdfpsIconPicker.setEnabled(true);
-            }
-            return true;
-        }
         return false;
-    }
-
-    @Override
-    public boolean onPreferenceTreeClick(Preference preference) {
-        if (preference == mCustomFPImage) {
-            Intent intent = new Intent(Intent.ACTION_PICK);
-            intent.setType("image/*");
-            startActivityForResult(intent, REQUEST_PICK_IMAGE);
-            return true;
-        }
-        return super.onPreferenceTreeClick(preference);
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent result) {
-       if (requestCode == REQUEST_PICK_IMAGE && resultCode == Activity.RESULT_OK) {
-           Uri uri = null;
-           if (result != null) {
-               uri = result.getData();
-               setPickerIcon(uri.toString());
-               Settings.System.putString(getContentResolver(), Settings.System.OMNI_CUSTOM_FP_ICON,
-                   uri.toString());
-            }
-        } else if (requestCode == REQUEST_PICK_IMAGE && resultCode == Activity.RESULT_CANCELED) {
-            // Do nothing, retain the previously set image, if any.
-        }
-    }
-
-    private void setPickerIcon(String uri) {
-        try {
-                ParcelFileDescriptor parcelFileDescriptor =
-                    getContext().getContentResolver().openFileDescriptor(Uri.parse(uri), "r");
-                FileDescriptor fileDescriptor = parcelFileDescriptor.getFileDescriptor();
-                Bitmap image = BitmapFactory.decodeFileDescriptor(fileDescriptor);
-                parcelFileDescriptor.close();
-                Drawable d = new BitmapDrawable(getResources(), image);
-                mCustomFPImage.setIcon(d);
-            }
-            catch (Exception e) {}
     }
 
     @Override
