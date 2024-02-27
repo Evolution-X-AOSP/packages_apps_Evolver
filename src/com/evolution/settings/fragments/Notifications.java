1c8fafaf3bb5d3c9afbeca9584039e6d24ff8a7a
Prathamesh Sahasrabuddhe <pratham220399@gmail.com>
2024-02-27 21:13:42 +0900

    Evolver: Bring back flashlight on call options [3/3]
    * Removed in 9c98e0244e6fbca4cf93529103b35cf31ec7f750.
    
    Add Flashlight blink on incoming calls [3/3]
    - Thanks to shripal17 for suggesting list preference
      instead of a toggle
    - Add a device specific flashlight check
    
    idoybh edits: The current check is for isRingerAudiable and
    not for DND - change var names and strings so it'll not mislead any
    future developer or any user
    
    Change-Id: I7d7d1522d4007320e8bb4d0794828c0b1330bdbb
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>
    Co-authored-by: Ido Ben-Hur <idoybh2@gmail.com>

--- src/com/evolution/settings/fragments/Notifications.java
+++ src/com/evolution/settings/fragments/Notifications.java
@@ -41,6 +41,11 @@ import com.android.settings.dashboard.DashboardFragment;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settingslib.search.SearchIndexable;
 
+import com.evolution.settings.preference.CustomSeekBarPreference;
+import com.evolution.settings.preference.SystemSettingListPreference;
+import com.evolution.settings.preference.SystemSettingSwitchPreference;
+import com.evolution.settings.utils.DeviceUtils;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -51,8 +56,22 @@ public class Notifications extends DashboardFragment implements
     private static final String TAG = "Notifications";
 
     private static final String ALERT_SLIDER_CAT = "alert_slider_cat";
+    private static final String FLASH_ON_CALL_OPTIONS = "on_call_flashlight_category";
+    private static final String FLASH_ON_NOTIFICATION_OPTIONS = "notification_flashlight_category";
+    private static final String PREF_FLASH_ON_CALL = "flashlight_on_call";
+    private static final String PREF_FLASH_ON_CALL_DND = "flashlight_on_call_ignore_dnd";
+    private static final String PREF_FLASH_ON_CALL_RATE = "flashlight_on_call_rate";
+    private static final String PREF_FLASH_ON_NOTIFY = "default_notification_torch";
+    private static final String PREF_FLASH_ON_NOTIFY_TIMES = "default_notification_torch1";
+    private static final String PREF_FLASH_ON_NOTIFY_RATE = "default_notification_torch2";
 
+    private CustomSeekBarPreference mFlashOnCallRate;
+    private CustomSeekBarPreference mFlashOnNotifyRate;
+    private CustomSeekBarPreference mFlashOnNotifyTimes;
     private PreferenceCategory mAlertSliderCat;
+    private SwitchPreference mFlashOnNotify;
+    private SystemSettingListPreference mFlashOnCall;
+    private SystemSettingSwitchPreference mFlashOnCallIgnoreDND;
 
     @Override
     protected int getPreferenceScreenResId() {
@@ -74,11 +93,113 @@ public class Notifications extends DashboardFragment implements
         if (!mAlertSliderAvailable) {
             prefScreen.removePreference(mAlertSliderCat);
         }
+
+        if (!DeviceUtils.deviceSupportsFlashLight(mContext)) {
+            PreferenceCategory flashOnCallCategory = (PreferenceCategory)
+                    findPreference(FLASH_ON_CALL_OPTIONS);
+            PreferenceCategory flashOnNotifCategory = (PreferenceCategory)
+                    findPreference(FLASH_ON_NOTIFICATION_OPTIONS);
+            prefScreen.removePreference(flashOnCallCategory);
+            prefScreen.removePreference(flashOnNotifCategory);
+        } else {
+            mFlashOnCallRate = (CustomSeekBarPreference)
+                    findPreference(PREF_FLASH_ON_CALL_RATE);
+            int value = Settings.System.getInt(resolver,
+                    Settings.System.FLASHLIGHT_ON_CALL_RATE, 1);
+            mFlashOnCallRate.setValue(value);
+            mFlashOnCallRate.setOnPreferenceChangeListener(this);
+
+            mFlashOnCallIgnoreDND = (SystemSettingSwitchPreference)
+                    findPreference(PREF_FLASH_ON_CALL_DND);
+            value = Settings.System.getInt(resolver,
+                    Settings.System.FLASHLIGHT_ON_CALL, 0);
+            mFlashOnCallIgnoreDND.setVisible(value > 1);
+            mFlashOnCallRate.setVisible(value != 0);
+
+            mFlashOnCall = (SystemSettingListPreference)
+                    findPreference(PREF_FLASH_ON_CALL);
+            mFlashOnCall.setSummary(mFlashOnCall.getEntries()[value]);
+            mFlashOnCall.setOnPreferenceChangeListener(this);
+
+            mFlashOnNotifyTimes = (CustomSeekBarPreference)
+                    findPreference(PREF_FLASH_ON_NOTIFY_TIMES);
+            mFlashOnNotifyRate = (CustomSeekBarPreference)
+                    findPreference(PREF_FLASH_ON_NOTIFY_RATE);
+            mFlashOnNotify = (SwitchPreference)
+                    findPreference(PREF_FLASH_ON_NOTIFY);
+            String strVal = Settings.System.getStringForUser(resolver,
+                    PREF_FLASH_ON_NOTIFY, UserHandle.USER_CURRENT);
+            final boolean enabled = strVal != null && !strVal.isEmpty();
+            mFlashOnNotify.setChecked(enabled);
+            updateFlashOnNotifyValues(enabled, strVal);
+            mFlashOnNotify.setOnPreferenceChangeListener(this);
+            mFlashOnNotifyTimes.setOnPreferenceChangeListener(this);
+            mFlashOnNotifyRate.setOnPreferenceChangeListener(this);
+        }
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mFlashOnCall) {
+            int value = Integer.parseInt((String) newValue);
+            Settings.System.putInt(resolver,
+                    Settings.System.FLASHLIGHT_ON_CALL, value);
+            mFlashOnCall.setSummary(mFlashOnCall.getEntries()[value]);
+            mFlashOnCallIgnoreDND.setVisible(value > 1);
+            mFlashOnCallRate.setVisible(value != 0);
+            return true;
+        } else if (preference == mFlashOnCallRate) {
+            int value = (Integer) newValue;
+            Settings.System.putInt(resolver,
+                    Settings.System.FLASHLIGHT_ON_CALL_RATE, value);
+            return true;
+        } else if (preference == mFlashOnNotify) {
+            boolean value = (Boolean) newValue;
+            if (!value) setFlashOnNotifyValues(0, 0);
+            else setFlashOnNotifyValues(2, 2);
+            return true;
+        } else if (preference == mFlashOnNotifyTimes) {
+            int value = (Integer) newValue;
+            setFlashOnNotifyValues(value, mFlashOnNotifyRate.getValue());
+            return true;
+        } else if (preference == mFlashOnNotifyRate) {
+            int value = (Integer) newValue;
+            setFlashOnNotifyValues(mFlashOnNotifyTimes.getValue(), value);
+            return true;
+        }
         return false;
+    }
+
+    private void updateFlashOnNotifyValues(boolean enabled) {
+        final String val = Settings.System.getStringForUser(
+                getActivity().getContentResolver(),
+                PREF_FLASH_ON_NOTIFY, UserHandle.USER_CURRENT);
+        updateFlashOnNotifyValues(enabled, val);
+    }
+
+    private void updateFlashOnNotifyValues(boolean enabled, String val) {
+        if (enabled) {
+            if (val.equals("1")) {
+                mFlashOnNotifyTimes.setValue(2);
+                mFlashOnNotifyRate.setValue(2);
+            } else {
+                String[] vals = val.split(",");
+                mFlashOnNotifyTimes.setValue(Integer.valueOf(vals[0]));
+                mFlashOnNotifyRate.setValue(Integer.valueOf(vals[1]));
+            }
+        }
+        mFlashOnNotifyTimes.setVisible(enabled);
+        mFlashOnNotifyRate.setVisible(enabled);
+    }
+
+    private void setFlashOnNotifyValues(int times, int rate) {
+        final boolean enabled = times != 0 && rate != 0;
+        String val = String.valueOf(times) + "," + String.valueOf(rate);
+        if (times == 2 && rate == 2) val = "1";
+        Settings.System.putStringForUser(getActivity().getContentResolver(),
+                PREF_FLASH_ON_NOTIFY, enabled ? val : null, UserHandle.USER_CURRENT);
+        updateFlashOnNotifyValues(enabled, val);
     }
 
     @Override
