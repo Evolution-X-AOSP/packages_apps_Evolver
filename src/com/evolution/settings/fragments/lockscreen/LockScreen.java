24eb7397b55fc6039e6fbde36788ca826adeadd0
Joey <joey@evolution-x.org>
2024-03-22 00:46:33 +0900

    Evolver: Adapt and clean up for A14 QPR2
    Co-authored-by: AnierinB <anierin@evolution-x.org>

---
+++ src/com/evolution/settings/fragments/lockscreen/LockScreen.java
@@ -0,0 +1,162 @@
+/*
+ * Copyright (C) 2023 Evolution X
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.evolution.settings.fragments.lockscreen;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.database.ContentObserver;
+import android.hardware.fingerprint.FingerprintManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemProperties;
+import android.os.UserHandle;
+import android.provider.SearchIndexableResource;
+import android.provider.Settings;
+import android.view.View;
+
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.SwitchPreferenceCompat;
+import androidx.preference.TwoStatePreference;
+
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.internal.util.evolution.udfps.CustomUdfpsUtils;
+
+import com.android.settings.R;
+import com.android.settings.Utils;
+import com.android.settings.dashboard.DashboardFragment;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settingslib.search.SearchIndexable;
+
+import com.evolution.settings.preference.SystemSettingListPreference;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SearchIndexable
+public class LockScreen extends DashboardFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    private static final String TAG = "LockScreen";
+
+    private static final String AOD_SCHEDULE_KEY = "always_on_display_schedule";
+    private static final String FINGERPRINT_CATEGORY = "lockscreen_fingerprint_category";
+    private static final String UDFPS_CATEGORY = "udfps_category";
+
+    private FingerprintManager mFingerprintManager;
+    private PreferenceCategory mFingerprintCategory;
+    private PreferenceCategory mUdfpsCategory;
+
+    static final int MODE_DISABLED = 0;
+    static final int MODE_NIGHT = 1;
+    static final int MODE_TIME = 2;
+    static final int MODE_MIXED_SUNSET = 3;
+    static final int MODE_MIXED_SUNRISE = 4;
+
+    private Preference mAODPref;
+
+    @Override
+    protected int getPreferenceScreenResId() {
+        return R.xml.evolution_settings_lockscreen;
+    }
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+
+        final ContentResolver resolver = getActivity().getContentResolver();
+        final PreferenceScreen prefScreen = getPreferenceScreen();
+        final PackageManager mPm = getActivity().getPackageManager();
+        final Resources res = getResources();
+
+        mFingerprintCategory = (PreferenceCategory) findPreference(FINGERPRINT_CATEGORY);
+        mFingerprintManager = (FingerprintManager) getActivity().getSystemService(Context.FINGERPRINT_SERVICE);
+        if (mPm.hasSystemFeature(PackageManager.FEATURE_FINGERPRINT) &&
+                 mFingerprintManager != null) {
+            if (!mFingerprintManager.isHardwareDetected()) {
+                prefScreen.removePreference(mFingerprintCategory);
+            }
+        }
+
+        mUdfpsCategory = findPreference(UDFPS_CATEGORY);
+        if (!CustomUdfpsUtils.hasUdfpsSupport(getContext())) {
+            prefScreen.removePreference(mUdfpsCategory);
+        }
+
+        mAODPref = findPreference(AOD_SCHEDULE_KEY);
+        boolean mAODAvailable = res.getBoolean(
+                com.android.internal.R.bool.config_dozeAlwaysOnDisplayAvailable);
+        if (!mAODAvailable) {
+            prefScreen.removePreference(mAODPref);
+        } else {
+            updateAlwaysOnSummary();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        updateAlwaysOnSummary();
+    }
+
+    private void updateAlwaysOnSummary() {
+        if (mAODPref == null) return;
+        int mode = Settings.Secure.getIntForUser(getActivity().getContentResolver(),
+                Settings.Secure.DOZE_ALWAYS_ON_AUTO_MODE, 0, UserHandle.USER_CURRENT);
+        switch (mode) {
+            default:
+            case MODE_DISABLED:
+                mAODPref.setSummary(R.string.disabled);
+                break;
+            case MODE_NIGHT:
+                mAODPref.setSummary(R.string.night_display_auto_mode_twilight);
+                break;
+            case MODE_TIME:
+                mAODPref.setSummary(R.string.night_display_auto_mode_custom);
+                break;
+            case MODE_MIXED_SUNSET:
+                mAODPref.setSummary(R.string.always_on_display_schedule_mixed_sunset);
+                break;
+            case MODE_MIXED_SUNRISE:
+                mAODPref.setSummary(R.string.always_on_display_schedule_mixed_sunrise);
+                break;
+        }
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        return false;
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.EVOLVER;
+    }
+
+    @Override
+    protected String getLogTag() {
+        return TAG;
+    }
+
+    public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+            new BaseSearchIndexProvider(R.xml.evolution_settings_lockscreen);
+}
