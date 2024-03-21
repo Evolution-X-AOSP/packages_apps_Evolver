b767a32f9ec534516e07d8283ae4df296d2453ee
Joey <joey@evolution-x.org>
2024-03-22 00:46:33 +0900

    Evolver: Adapt to A14 QPR2

---
+++ src/com/evolution/settings/fragments/CarrierLabel.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright (C) 2019 AospExtended
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.evolution.settings.fragments;
+
+import android.app.AlertDialog;
+import android.content.ContentResolver;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.text.InputFilter;
+import android.text.Spannable;
+import android.text.TextUtils;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.SwitchPreferenceCompat;
+import androidx.preference.TwoStatePreference;
+
+import com.android.internal.logging.nano.MetricsProto;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.evolution.settings.preference.SystemSettingPrimarySwitchPreference;
+
+public class CarrierLabel extends SettingsPreferenceFragment
+        implements Preference.OnPreferenceClickListener {
+
+    public static final String TAG = "CarrierLabel";
+
+    private SystemSettingPrimarySwitchPreference mCarrierLabel;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.carrier_label);
+
+        mCarrierLabel = (SystemSettingPrimarySwitchPreference) findPreference("enable_custom_carrier_label");
+        mCarrierLabel.setOnPreferenceClickListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceClick(Preference preference) {
+        if (preference == mCarrierLabel) {
+            ContentResolver resolver = getActivity().getContentResolver();
+            AlertDialog.Builder alert = new AlertDialog.Builder(getActivity());
+            alert.setTitle(R.string.carrier_label_custom_title);
+            alert.setMessage(R.string.carrier_label_custom_explain);
+            LinearLayout container = new LinearLayout(getActivity());
+            container.setOrientation(LinearLayout.VERTICAL);
+            LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.WRAP_CONTENT);
+            lp.setMargins(55, 20, 55, 20);
+            final EditText input = new EditText(getActivity());
+            int maxLength = 25;
+            input.setLayoutParams(lp);
+            input.setGravity(android.view.Gravity.TOP | Gravity.START);
+            String carrierLabel = Settings.System.getStringForUser(getContentResolver(),
+                    Settings.System.CUSTOM_CARRIER_LABEL, UserHandle.USER_CURRENT);
+            input.setText(TextUtils.isEmpty(carrierLabel) ? "" : carrierLabel);
+            input.setFilters(new InputFilter[] {new InputFilter.LengthFilter(maxLength)});
+            container.addView(input);
+            alert.setView(container);
+            alert.setPositiveButton(getString(android.R.string.ok),
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        String value = ((Spannable) input.getText()).toString().trim();
+                        Settings.System.putStringForUser(resolver,
+                                Settings.System.CUSTOM_CARRIER_LABEL,
+                                value, UserHandle.USER_CURRENT);
+                    }
+            });
+            alert.setNegativeButton(getString(android.R.string.cancel), null);
+            alert.show();
+        }
+        return true;
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.EVOLVER;
+    }
+}
